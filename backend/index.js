4c5fdcec74981f58b5bf4603ca2cd1462491fcd6
Ishan <ishan28291@gmail.com>
2024-06-07 23:01:55 +0530

    serve static frontend from express in prod

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,16 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.js';
 import errorHandler from './middlewares/errorHandler.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
+
+if (process.env.ENVIRONMENT === 'production')
+  app.use(express.static(path.join(__dirname, '..', 'frontend/dist')));
 
 app.use(express.json());
 app.use(cookieParser());
@@ -14,6 +20,11 @@ app.use(cookieParser());
 app.use('/user', userRoutes);
 
 app.use(errorHandler);
+
+if (process.env.ENVIRONMENT === 'production')
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'frontend/dist', 'index.html'));
+  });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port: ', process.env.PORT || 3000);
